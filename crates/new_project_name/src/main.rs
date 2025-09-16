da5450dc382b53ef86e471f34b59276e6cef8737
ntjohnson1 <24689722+ntjohnson1@users.noreply.github.com>
2025-09-16 07:10:00 -0400

    Prune template

--- crates/new_project_name/src/main.rs
+++
@@ -1,5 +0,0 @@
-//! Example of a Rust binary.
-
-fn main() {
-    println!("Hello, new_project_name!");
-}
---
+++ lerobot_preview/__init__.py
@@ -0,0 +1,5 @@
+"""Package To Download and View Cloud Hosted LeRobot Datasets."""
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
